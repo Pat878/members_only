f81e1eeefdef3ac954540c45ea02cd598bdc77e3
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-10-02 18:38:00 -0400

    Update database with password_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002221221) do
+ActiveRecord::Schema.define(version: 20171002223552) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171002221221) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
