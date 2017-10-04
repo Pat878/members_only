8546062d1eee2d9362a69f60aa1f9d22b6c8c45a
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-10-04 19:18:39 -0400

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
   end
 
   def create
+    @post = Post.new(user_params)
+
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
+    end
 end
