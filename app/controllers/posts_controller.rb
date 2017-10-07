f1a83c357cca4760013eb4b8acc91e3fb269645e
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-10-07 10:05:00 -0400

    Add before_action method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+
   def new
     @post = Post.new
   end
@@ -21,5 +23,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:body, :user_id)
-    end
+  end
+
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
 end
